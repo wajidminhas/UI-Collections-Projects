250795731f59ba3982d80cc5328d4dc86940a86d
wajidminhas <shanitent667@gmail.com>
2025-08-24 20:10:17 +0500

    navbar and hero section added

--- product-service/app/layout.tsx
+++ product-service/app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="container mx-auto px-4">
         {children}
+
+        </div>
       </body>
     </html>
   );
